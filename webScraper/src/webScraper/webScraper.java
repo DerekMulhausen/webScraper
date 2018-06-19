8aa07c28985a5cce009d7ba3d9f0983e45e6a327
DMulhausen <DMulhausen@KPLAXLT0286.alpha.kaplaninc.com>
2018-06-19 18:14:36 -0500

    first commit

---
+++ webScraper/src/webScraper/webScraper.java
@@ -0,0 +1,10 @@
+package webScraper;
+
+public class webScraper {
+	//Program will scrape a web page for raw html
+	//Program will scrape metadata of photos, videos, and other media
+	//Program will store URL data and have the ability to collect and display data
+	//Program will take URL, strings to target, and strings to omit
+	//Program will have the ability to store data found as well as setup data of strings to target and strings to omit
+	
+}
