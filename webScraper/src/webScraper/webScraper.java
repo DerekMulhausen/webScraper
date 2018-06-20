6374b0aae7c059326ffba084e0787dc57b05305f
DMulhausen <DMulhausen@KPLAXLT0286.alpha.kaplaninc.com>
2018-06-20 15:29:58 -0500

    working on HTML collection

--- webScraper/src/webScraper/webScraper.java
+++ webScraper/src/webScraper/webScraper.java
@@ -1,5 +1,8 @@
 package webScraper;
 
+import org.jsoup.*;
+import org.jsoup.examples.*;
+
 public class webScraper {
 	//Program will scrape a web page for raw html
 	//Program will scrape metadata of photos, videos, and other media
@@ -7,4 +10,18 @@ public class webScraper {
 	//Program will take URL, strings to target, and strings to omit
 	//Program will have the ability to store data found as well as setup data of strings to target and strings to omit
 	
+	public static void main(String[] args)
+	{
+		String content;
+		try {
+			content = URLConnectionReader.getText("https://en.wikipedia.org/wiki/\"Hello,_World!\"_program");
+			content=new org.jsoup.examples.HtmlToPlainText().getPlainText(Jsoup.parse(content));
+			System.out.print(content);	
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
 }
